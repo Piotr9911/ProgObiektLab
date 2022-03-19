4a2c50b980fc9a8f2f124ac71e426851a9f79215
Piotr9911 <91785110+Piotr9911@users.noreply.github.com>
2022-03-19 17:12:12 +0100

    Update Program.cs

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -1,12 +1,71 @@
-﻿using System;
-
-namespace Lab02
+﻿namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Teacher treacher = new Teacher("Maria Skłodowska", 50);
+
+            Student student1 = new Student("Jan Kowaslski", 21, "LAB-01");
+            Student student2 = new Student("Jan Kowaslski", 21, "LAB-01");
+            Student student3 = new Student("Jaś Fasola", 23, "LAB-02");
+
+            /*student1.AddTask("Taks A", TaskStatus.Waiting);
+            student1.AddTask("Taks B", TaskStatus.Waiting);
+            student1.AddTask("Taks C", TaskStatus.Rejected);
+
+            student2.AddTask("Taks A", TaskStatus.Waiting);
+            student2.AddTask("Taks B", TaskStatus.Waiting);
+            student2.AddTask("Taks C", TaskStatus.Rejected);
+
+            student3.AddTask("Taks D", TaskStatus.Done);
+            student3.AddTask("Taks E", TaskStatus.Waiting);
+            student3.AddTask("Taks F", TaskStatus.Waiting);
+
+            student3.UpdateTask(1, TaskStatus.Done);
+            student3.UpdateTask(2, TaskStatus.Progress);
+
+            Person[] persons = { treacher, student1, student2, student3 };
+            Classroom classroom = new Classroom("Sala Komputerowa", persons);
+
+            Console.WriteLine(classroom);
+
+            Console.WriteLine("student1 == student2: " + student1.Equals(student2)); // Output: student1 == student2: true
+            Console.WriteLine("student1 == student3: " + student1.Equals(student3)); // Output: student1 == student3: false*/
+        }
+        public class Person
+        {
+            private string name;
+            private int age;
+            public Person(string name, int age)
+                {
+                this.name = name;
+                string v = age.ToString();
+
+                }
+        }
+
+        public class Teacher : Person
+        {
+            public Person treacher { get; set;}
+
+            public Teacher(string name, int age) :  base(name, age)
+            {
+
+            }
+
+        }
+        public class Student : Person
+        {
+            public Person student1 { get; set; }
+            public Person student2 { get; set; }
+            public Person student3 { get; set; }
+
+            public Student(string name, int age, string v) : base(name,age)
+            {
+
+            }
+            
         }
     }
 }
